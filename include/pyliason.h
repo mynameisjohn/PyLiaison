7c252874a79cadd31327915a648a04eb5536cb97
mynameisjohn <mynameisjohnj@gmail.com>
2016-03-12 17:04:52 -0500

    doxygen stuff for pyliaison.h

--- include/pyliason.h
+++ include/pyliason.h
@@ -29,43 +29,48 @@
 // used to expose functions and objects
 // to the python interpreter
 
-#include <mutex>
-
 #include "pyl_misc.h"
 #include "pyl_module.h"
 
 namespace pyl
 {
-    // Mutex that gets locked when the interpreter runs simple commands
-    // TODO there are plenty of commands other than PyRun_SimpleString
-    // that would require a lock, so I'm not sure how you want to handle it
-	extern std::mutex CmdMutex;
-	
-    // Initialize python interpreter, load module, define any registered classes
+	/********************************************//*!
+	pyl::initialize
+	\brief Initialize the python interpreter and all custom modules
+
+	This function closes any pre-existing python interpreter (via Py_Finalize)
+	and creates all python modules declared as pyl::ModuleDef instances via py::Module::InitAllModules. 
+	Once that is done a call to Py_Initialize is made. 
+	***********************************************/
 	void initialize();
     
-    // Finalize python interpreter
+	/********************************************//*!
+	pyl::finalize
+	\brief Finalize the python interpreter via Py_Finalize
+	***********************************************/
 	void finalize();
     
-    // PyErr_Print/Clear, PyObject_Print
 	void print_error();
 	void clear_error();
 	void print_object(PyObject *obj);
 
-    // This gets called when the custom module is loaded
-	PyMODINIT_FUNC _Mod_Init();
+	/********************************************//*!
+	pyl::RunCmd
+	\brief Executes a string command (in python) as if it was run via console
 
-    // Thread safe way of running a python command
-	inline int RunCmd(std::string cmd) {
-		std::lock_guard<std::mutex> lg(CmdMutex);
+	\param[in] cmd The string based python command
+	\param[out] ret The integer returned by PyRun_SimpleString
+	***********************************************/
+	int RunCmd(std::string cmd) {
 		return PyRun_SimpleString(cmd.c_str());
 	}
 	
-    // Invokes the above with a file from disk
-    int RunFile(std::string fileName);
+	/********************************************//*!
+	pyl::RunFile
+	\brief Executes a python script file on disk
 
-	// Get the PyLiaison Module object
-	// TODO Make an arbitrary number of modules
-	// and have them gettable by clients
-	//Object GetPyLiaisonModule();
+	\param[in] fileName The full filename of the script
+	\param[out] ret The integer returned by PyRun_SimpleString
+	***********************************************/
+    int RunFile(std::string fileName);
 }
