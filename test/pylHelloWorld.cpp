79cd7973617e4cec2a4c557ead34a603fc2a2f4c
mynameisjohn <mynameisjohnj@gmail.com>
2016-06-26 13:04:33 -0400

    Adding a hello world example

---
+++ test/pylHelloWorld.cpp
@@ -0,0 +1,55 @@
+#include <pyliaison.h>
+#include <iostream>
+
+int main( int argc, char ** argv )
+{
+	// We may get an exception from the interpreter if somethign is amiss
+	try
+	{
+		// Initialize the python interpreter
+		pyl::initialize();
+
+		// Get the main module, which is stored as python object
+		// (these are backed by std::shared_ptr<PyObject>)
+		pyl::Object obMain = pyl::GetMainModule();
+
+		// You can execute commands in the interpreter via RunCmd
+		pyl::RunCmd( "print('Hello from Python!')" );
+
+		// Import sys, get the interpeter version
+		pyl::RunCmd( "import sys" );
+		pyl::RunCmd( "print(sys.version_info)" );
+
+		// Declare a string in the main module called stringFromCPP
+		// and print it in the interpreter
+		obMain.set_attr( "stringFromCPP", "Hello World from C++!" );
+		pyl::RunCmd( "print(stringFromCPP)" );
+
+		// Create a variable in the main module called iVal, storing
+		// an integer value 12345, and print out the value
+		int iVal( 12345 );
+		obMain.set_attr( "iVal", iVal );
+		pyl::RunCmd( "print('We got', iVal, 'from CPP')" );
+
+		// Negate the value in python
+		pyl::RunCmd( "iVal = -iVal" );
+
+		// Get the value and convert it to an int
+		// (conversions return false if they fail)
+		int iNegVal( 0 );
+		if ( obMain.get_attr( "iVal", iNegVal ) == false || iNegVal != -iVal )
+			throw pyl::runtime_error( "Error getting iVal back!" );
+		std::cout << "We sent " << iVal << ", we got " << iNegVal << " back!" << std::endl;
+
+		// Shut down the interpreter
+		pyl::finalize();
+
+		return EXIT_SUCCESS;
+	}
+	// These exceptions are thrown when something in pyliaison
+	// goes wrong, but they're a child ofof std::runtime_error
+	catch ( pyl::runtime_error )
+	{
+		return EXIT_FAILURE;
+	}
+}
